d716c94019294ce97f1711d0d011fbbd61a85154
thjxs <tanhongjianx@163.com>
2020-05-09 14:53:30 +0800

    Add meet date

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="relative">
-    <div class="hover absolute">
-      <div id="together" class="absolute">
-        <p>{{ together }}</p>
+    <div class="absolute right-0 mr-1">
+      <div class="hover w-4">
+        <div id="together" class="absolute content opacity-0 w-4">
+          <p>{{ together }}</p>
+        </div>
+      </div>
+      <div class="hover w-4">
+        <div id="meet" class="absolute content opacity-0 w-4">
+          <p>{{ meet }}</p>
+        </div>
       </div>
     </div>
     <div class="text-center text-white text-5xl">Goto</div>
@@ -14,12 +21,14 @@
 import { getCountdown } from '../utils';
 const targetDate = new Date('2020-06-20T00:00:00');
 const startDate = new Date('2019-09-06T00:00:00');
+const meetDate = new Date('2019-05-27T00:00:00');
 
 export default {
   data: function() {
     return {
       countdown: getCountdown(targetDate),
       together: getCountdown(startDate),
+      meet: getCountdown(meetDate),
       countdownTimer: null
     };
   },
@@ -27,6 +36,7 @@ export default {
     this.countdownTimer = setInterval(() => {
       this.countdown = getCountdown(targetDate);
       this.together = getCountdown(startDate);
+      this.meet = getCountdown(meetDate);
     }, 1000 * 60 * 60);
   },
   destroyed: function() {

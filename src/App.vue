ce893c62bbefb50521cf95a3dee342c091d1a23a
thjxs <tanhongjianx@163.com>
2020-07-06 18:18:59 +0800

    Add cat, dog

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <clock />
     <countdown />
+    <drawer style="right: 223px; top: 120px;">
+      <img src="./assets/dog_t.png" alt="" srcset="">
+    </drawer>
     <div class="fixed" style="left: 531px;top: 578px;">
       <img style="width: 120px;" src="./assets/dalao.png" alt="">
     </div>
@@ -14,18 +17,23 @@
     <div class="absolute" style="left: 1180px;">
       <img src="./assets/lc.png" style="width: 400px;" alt="">
     </div>
+    <div class="absolute" style="left: 210px;top: 353px;">
+      <img style="width: 260px;" src="./assets/back.png" alt="">
+    </div>
   </div>
 </template>
 
 <script>
 import Clock from './components/Clock.vue';
 import Countdown from './components/Countdown.vue';
+import Drawer from './components/Drawer.vue'
 
 export default {
   name: 'App',
   components: {
     Clock,
-    Countdown
+    Countdown,
+    Drawer
   }
 };
 </script>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="fixed" :style="style">
+    <div class="w-12 hover">
+        <div class="slot">
+            <slot></slot>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    props: ['style']
+}
+</script>
+<style scoped>
+.slot {
+    width: 0;
+    opacity: 0.3;
+}
+.hover:hover .slot {
+    display: block;
+    width: 233px;
+    opacity: 1;
+    transition: all .3s ease-in;
+}
+</style>
